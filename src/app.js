49f5b55764a2a47dd3ce94647cd47741ff8302e0
Diana Stikheeva <d.stikheeva@gmail.com>
2023-01-11 12:54:49 +0500

    feat: add JSDoc

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+/**
+ * Успеваемость учеников
+ * @param {Object} scores
+ * @param {string} scores.key - Имя ученика
+ * @param {number} scores[key] - Успеваемость ученика
+ */
 const scores = {
     Anna: 10,
     Olga: 1,
     Ivan: 5,
 };
 
+
+/**
+ * Подсчет суммы всех баллов
+ * @returns {number}
+ */
 const getScore = () => {
     let scoresSum = 0;
     for (let key in scores) {
@@ -11,5 +22,6 @@ const getScore = () => {
       }
     return scoresSum;
 };
+export default getScore;
 
 console.log(getScore());
